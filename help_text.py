fb48ee396149f884e43ff48e778a3e34f8d102d9
Matias Mutuan <matiastm99@gmail.com>
2023-04-15 23:55:42 -0300

    Man page

---
+++ help_text.py
@@ -0,0 +1,12 @@
+def print_help():
+    print("How to use:")
+    print("Enter the operation followed by the numbers.")
+    print("EXAMPLE: add 1 2 3")
+
+    print("The following operations are available:")
+    print("Addition: add")
+    print("Substraction: sub")
+    print("Multiplication: mult")
+    print("Division: div\n")
+
+    print("Press q to exit.")
