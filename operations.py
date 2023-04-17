1781557b3f1b15c6d78c60cd84b71ce60eb08f79
Matias Mutuan <matiastm99@gmail.com>
2023-04-17 19:38:46 -0300

    Add function explanation

--- operations.py
+++ operations.py
@@ -1,3 +1,22 @@
+"""
+Why int(numbers[0])?
+First: numbers[0], this is because if we have initialize result as 0, the output
+of the operation will change drastically. 
+Example: numbers = (4, 2, 1), expected output = 1.
+result = 0 - 4 = -4
+result = -4 - 2 = -6
+result = -6 - 1 = -7. Wrong output.
+
+With result = int(numbers[0]) = result = 4.
+(numbers is a tuple of strings, so type casting is necessary).
+result = 4 - 2 = 2
+result = 2 - 1 = 1. Correct output.
+
+Second: numbers = numbers[1:], since the first value of the input is assigned to result,
+we have to remove it to avoid duplication, and since numbers is a tuple,
+slices are the way to go.
+"""
+
 def add(numbers):
     result = int(numbers[0])
     numbers = numbers[1:]
@@ -25,4 +44,3 @@ def div(numbers):
     for number in numbers:
         result /= int(number)
     return result
-    
