f9853f06b7c6c37d9d5c47b46a50d5db3fb2a9d1
Matias Mutuan <matiastm99@gmail.com>
2023-04-17 19:37:47 -0300

    Remove unnecessary text

--- help_text.py
+++ help_text.py
@@ -1,12 +1,6 @@
 def print_help():
-    print("How to use:")
-    print("Enter the operation followed by the numbers.")
-    print("EXAMPLE: add 1 2 3")
-
     print("The following operations are available:")
     print("Addition: add")
     print("Substraction: sub")
     print("Multiplication: mult")
     print("Division: div\n")
-
-    print("Press q to exit.")
