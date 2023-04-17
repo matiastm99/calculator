4bcc1f2c85f59a5ead3cf3df5f99e0d7f76e8b7c
Matias Mutuan <matiastm99@gmail.com>
2023-04-17 19:42:10 -0300

    Upgrade
    Add simple guide text
    
    Modify if statement

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from operations import add, sub, mult, div
 from help_text import print_help
 
 def main():
+    print("Type h or help to see available options.")
+    print("Press q to quit.\n")
+
     while True:
-        option = input()
+        option = input("Enter an operation: ")
         if option == "q":
             break
-        if option == "help":
+        
+        if (option == "h") or (option == "help"):
             print_help()
         else:
-            numbers = input().split()
+            numbers = input("Enter some numbers: ").split()
 
             if option == "add":
                 print(add(numbers))
