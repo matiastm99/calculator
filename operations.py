2c078308cb16e2f7df22c551a79bbe1de6c3ef21
Matias Mutuan <matiastm99@gmail.com>
2023-04-18 19:03:47 -0300

    Various changes
    Fix grammar (i think)
    
    Add function annotation

--- operations.py
+++ operations.py
@@ -1,6 +1,6 @@
 """
 Why int(numbers[0])?
-First: numbers[0], this is because if we have initialize result as 0, the output
+First: numbers[0], this is because if we had initialized result as 0, the output
 of the operation will change drastically. 
 Example: numbers = (4, 2, 1), expected output = 1.
 result = 0 - 4 = -4
@@ -17,28 +17,28 @@ we have to remove it to avoid duplication, and since numbers is a tuple,
 slices are the way to go.
 """
 
-def add(numbers):
+def add(numbers: tuple[str]) -> int:
     result = int(numbers[0])
     numbers = numbers[1:]
     for number in numbers:
         result += int(number)
     return result 
 
-def sub(numbers):
+def sub(numbers: tuple[str]) -> int:
     result = int(numbers[0])
     numbers = numbers[1:]
     for number in numbers:
         result -= int(number)
     return result
 
-def mult(numbers):
+def mult(numbers: tuple[str]) -> int:
     result = int(numbers[0])
     numbers = numbers[1:]
     for number in numbers:
         result *= int(number)
     return result
 
-def div(numbers):
+def div(numbers: tuple[str]) -> int:
     result = int(numbers[0])
     numbers = numbers[1:]
     for number in numbers:
